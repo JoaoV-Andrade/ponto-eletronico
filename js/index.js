e5dbabf8bbba8e25b6673c8cd908be20b56ddc64
JoaoV-Andrade <joaovitor1110@gmail.com>
2024-09-05 10:43:58 -0300

    aula 05/09

--- js/index.js
+++ js/index.js
@@ -1,19 +1,51 @@
-const diaSemana1 = document.getElementById("diaSemana")
-const diaMesAno1 = document.getElementById("diaMesAno")
-const hora1 = document.getElementById("hora")
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano");
+const horaMinSeg = document.getElementById("hora-min-seg");
+const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sabado"]
 
-// todo cojunto numérico (exceo o ano) deve ter 2 digitos
-// retornar dia da semana por extenso
+
+const dialogPonto = document.getElementById("dialog-ponto");
+
+
+const regPonto = document.getElementById("reg-Ponto");
+regPonto.addEventListener('click', () => {
+    dialogPonto.showModal();
+});
+
+
+const dialogFechar = document.getElementById("dialog-fechar");
+dialogFechar.addEventListener('click',()=>{
+    dialogPonto.close();
+});
+
+
+
+
+// Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
+// Retornar dia da semana por extenso (em pt-br)
+
+function daySemana() {
+    //retornar dia da semana
+    const date = new Date();
+    return arrayDayWeek[date.getDay()];
+}
 
 function dataCompleta() {
     const date = new Date();
-    return date.getDate() + "/" + (date.getMonth() + 1) +"/" + date.getFullYear();
- }
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
 
-function horaCompleta(){
+function horaCompleta() {
     const date = new Date();
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
- diaMesAno1.textContent = dataCompleta();
- hora1.textContent = horaCompleta();
+function atualizaHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
+
+
+setInterval(atualizaHora, 1000);
+
+diaSemana.textContent = daySemana();
+diaMesAno.textContent = dataCompleta();
