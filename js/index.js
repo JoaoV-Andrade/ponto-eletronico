ac3d2cee6a66dbe2b1957c4254a6e5202cef05aa
JoaoV-Andrade <joaovitor1110@gmail.com>
2024-09-12 10:44:39 -0300

    aula 12/09

--- js/index.js
+++ js/index.js
@@ -7,6 +7,17 @@ const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Q
 const dialogPonto = document.getElementById("dialog-ponto");
 
 
+
+
+navigator.geolocation.getCurrentPosition((position) =>{
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+});
+
+
+
+
 const regPonto = document.getElementById("reg-Ponto");
 regPonto.addEventListener('click', () => {
     dialogPonto.showModal();
@@ -18,14 +29,36 @@ dialogFechar.addEventListener('click',()=>{
     dialogPonto.close();
 });
 
+const btnDialogRegistrarPonto = document.getElementById("btn-dialog-registrar-ponto");
+btnDialogRegistrarPonto.addEventListener("click", ()=>{
 
+    let data = dataCompleta();
+    let hora = horaCompleta();
+    let tipoPonto = document.getElementById("select-tipos-ponto").value;
 
+    let ponto = {
+        "data": data,
+        "hora": hora,
+        "tipo": tipoPonto,
+        "id": 1
+    }
 
-// Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
-// Retornar dia da semana por extenso (em pt-br)
+    localStorage.setItem("registro",JSON.stringify(ponto));
+
+    console.log(ponto);
+    
+
+    //someto o ulimo resgistro esta sendo salvo
+    //como resolver isso , de modo que eu persista todos os pontos?
+});
+
+// salvar o ultimo tipo de ponto registrado pelo usuario
+// fazer o select considerar o ultim ponto e selcioar, por padrao
+//o proximo possivel ponto de usuário
+
 
 function daySemana() {
-    //retornar dia da semana
+    
     const date = new Date();
     return arrayDayWeek[date.getDay()];
 }
